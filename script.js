18db8f61425d279bac0b67e9cbc4638400123ce2
Pavel Trushnikov <oracal@mail.ru>
2021-12-10 00:00:35 +0300

    Выполнение задания одиннадцатого урока

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
+const btn = document.getElementById('btn');
+const inputText = document.getElementById('text');
+const square = document.getElementById('square');
+const ebtn = square.querySelector('#e_btn');
+const circle = square.querySelector('#circle');
+const range = document.getElementById('range');
+const rangeSpan = document.getElementById('range-span');
 
+rangeSpan.textContent = range.value;
 
+btn.addEventListener('click', () => {
+    square.style.backgroundColor = inputText.value;
+    ebtn.style.display = 'none';
+});
 
+range.addEventListener('input', () => {
+    rangeSpan.textContent = range.value;
+    circle.style.height = range.value + '%';
+    circle.style.width = range.value + '%';
+});
 
-
+console.log(range);
+console.log(rangeSpan);
